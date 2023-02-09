a0b5b80a668fbdfdc89bf1b7224b730d2f4f9282
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-10 02:20:51 +0900

    feat: 버튼 샘플 개발

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import '../../assets/text-style.css'
-export default function Button({ css, content }) {
-  return <button className={css}>{content}</button>
+export default function Button({ css, content, onClick }) {
+  return (
+    <button className={css} onClick={onClick}>
+      {content}
+    </button>
+  )
 }
