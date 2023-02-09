d46a19dfd638a8812d7a505e2819dd6a4071eaea
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-09 15:14:39 +0900

    feat: 버튼 컴포넌트

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import '../../assets/text-style.css'
-export default function Button() {
-  return (
-    <button className='Btn-Poppins-M-30 bg-white text-black rounded-[48px] w-56 h-20'>
-      Start
-    </button>
-  )
+export default function Button({ css, content }) {
+  return <button className={css}>{content}</button>
 }
