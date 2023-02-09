e443e6b522d0cdaa1bedb210238fc4e8ed4d3cf1
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-09 15:15:59 +0900

    Update MainLogo.jsx

--- src/components/common/MainLogo.jsx
+++ src/components/common/MainLogo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ReactComponent as IfIWere } from '../../assets/MovCard.svg'
-export default function MainLogo({ value }) {
+export default function MainLogo({ value, css }) {
   const { width, height } = value
-  return <IfIWere width={width} height={height}></IfIWere>
+  return <IfIWere width={width} height={height} className={css}></IfIWere>
 }
