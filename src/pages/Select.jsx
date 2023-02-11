35301054666b4c57b65519549b498f438f88c37b
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-11 23:15:06 +0900

    style: 카드 위치 변경

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -1,15 +1,36 @@
-import React, { useEffect } from 'react'
-import background from '../assets/selectBackground.png'
+import React, { useEffect, useState } from 'react'
+import background from '../assets/selectBackground.svg'
 import MainLogo from '../components/common/MainLogo'
 import Card from '../components/common/Card'
+import BackSpace from '../components/common/BackSpace'
+
 export default function Select() {
   useEffect(() => {
     const root = document.getElementById('root')
     root.style.background = `url(${background})`
     root.style.backgroundSize = 'cover'
   }, [])
+  const [qa, setQa] = useState([
+    {
+      title: '안녕',
+      display: true,
+    },
+    {
+      title: '하세요',
+      display: true,
+    },
+    {
+      title: '반갑',
+      display: false,
+    },
+    {
+      title: '습니다',
+      display: false,
+    },
+  ])
   return (
-    <div className='w-4/5  flex flex-col'>
+    <div className='w-4/5 flex flex-col static'>
+      <BackSpace />
       <MainLogo
         value={{
           width: 123.41,
@@ -20,7 +41,7 @@ export default function Select() {
       <p className='Pretendard-M-25 h-1/6 flex justify-center items-center'>
         둘중 더 원하는 카드를 클릭 하세요.
       </p>
-      <div className='mx-auto h-3/6 w-5/6 flex  justify-evenly items-center'>
+      <div className='mx-auto h-3/6 w-5/6 flex  justify-center items-center gap-20'>
         <Card />
         <Card />
       </div>
