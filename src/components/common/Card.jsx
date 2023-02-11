d9be624f491bdb56c8862d048fe9268975ec979f
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-11 23:14:27 +0900

    style: 카드 trasition 효과 추가

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { ReactComponent as ExCard } from '../../assets/excard.svg'
+import '../../assets/text-style.css'
 export default function Card() {
   return (
-    <div className='card w-72 h-96 flex flex-col justify-evenly'>
-      <ExCard className='mx-auto'></ExCard>
-      <p className='text-center text-black'>발자국이 많이 찍힌 눈길</p>
-    </div>
+    <>
+      <div className='card w-72 h-[25.625rem] flex flex-col justify-evenly hover:-translate-y-8 transition delay-75 duration-300 ease-in-out '>
+        <ExCard className='mx-auto mb-10'></ExCard>
+        <p className='Pretendard-M-23 text-center text-black p-2.5 mb-10'>
+          발자국이 많이 찍힌 눈길
+        </p>
+      </div>
+    </>
   )
 }
