ac5f5a321ba8cbd839a5525e36b3c2f7319e277b
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-17 16:18:03 +0900

    style: 디자인 변경

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -7,21 +7,21 @@ export default function Card({ info, changeQa }) {
     'card w-72 h-[25.625rem] flex flex-col justify-evenly hover:-translate-y-8 transition delay-75 duration-300 ease-in-out '
   let css = display ? style : style + 'hidden'
   const handler = () => {
-    const card = document.getElementsByClassName('card')[id]
-    card.style.transition = `ease 1s`
-    card.style.transform = `scale(0.7)`
-    setTimeout(() => {
-      card.style.transform = `scale(1)`
-      changeQa(stage, title)
-    }, 900)
+    const card = document.getElementsByClassName('card')
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.transition = `ease 1s`
+      card[i].style.transform = `translateY(500px)`
+      setTimeout(() => {
+        card[i].style.transform = `translateY(0px)`
+        changeQa(stage, title)
+      }, 900)
+    }
   }
   return (
     <>
       <div className={css} onClick={handler}>
         <ExCard className='mx-auto mb-10'></ExCard>
-        <p className='Pretendard-M-23 text-center text-black p-2.5 mb-10'>
-          {title}
-        </p>
+        <p className='Pretendard-M-23 text-center p-2.5 mb-10'>{title}</p>
       </div>
     </>
   )
