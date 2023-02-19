a7c1bde9a8d43dc12c3f45f6a136e4284ec955d3
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-19 15:22:15 +0900

    style: 로딩 text, css 변경

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className='bg-[#111220] w-full h-screen'>
       <div className='ring text-white'>
-        Loading
+        Loading...
         <span></span>
       </div>
     </div>
