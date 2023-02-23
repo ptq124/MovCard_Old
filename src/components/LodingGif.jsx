4ee1ffc6826ffa7709e49d96add99eed0e334804
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-24 04:43:43 +0900

    feat: 로딩 컴포넌트 개발

---
+++ src/components/LodingGif.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import MainLogo from './common/MainLogo'
+export default function LodingGif() {
+  useEffect(() => {
+    const root = document.getElementById('root')
+    root.style.background = ''
+    console.log('loading..')
+  }, [])
+  return (
+    <div className='bg-[#111220] w-full h-screen absolute right-0'>
+      <div className='w-full h-screen flex flex-col items-center justify-center'>
+        <MainLogo
+          value={{
+            width: 342,
+            height: 163,
+          }}
+          css={'h-1/4'}
+        ></MainLogo>
+        <p>당신의 취향을 분석하고 있어요</p>
+      </div>
+    </div>
+  )
+}
