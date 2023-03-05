8b33bce160673298dbabeb689a0a371308598967
박태규 Park Tae Kyu <ptq124@naver.com>
2023-03-05 21:15:32 +0900

    feat: 새로고침 기능 구현

--- src/components/common/Refresh.jsx
+++ src/components/common/Refresh.jsx
@@ -5,7 +5,12 @@ export default function Refresh({ css }) {
   const style =
     'Btn-Poppins-M-27 h-14 w-[11.5rem] flex justify-center items-center gap-2.5 rounded-[47px] border border-solid border-[#ffffff]'
   return (
-    <button className={style + css}>
+    <button
+      className={style + css}
+      onClick={() => {
+        window.location.replace('/')
+      }}
+    >
       <RefreshIcon />
       Restart
     </button>
